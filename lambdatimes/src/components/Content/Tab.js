81e929b396dc20434a58ff4e22adfa65256cffc2
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-01-18 12:34:16 -0500

    Refactored all CSS to Styled Components

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,15 +1,38 @@
 import React from "react";
 import { PropTypes } from "prop-types";
+import styled from "styled-components";
 
+const TabStyled = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+  &:hover {
+    text-decoration: underline;
+  }
+  .active-tab {
+    background-color: #fff;
+    color: #333;
+    border: 2px solid #333;
+  }
+`;
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
 
   return (
-    <div className="tab" onClick={() => props.selectedTabHandler(props.tab)}>
+    <TabStyled onClick={() => props.selectedTabHandler(props.tab)}>
       {props.tab.toUpperCase()}
-    </div>
+    </TabStyled>
   );
 };
 
